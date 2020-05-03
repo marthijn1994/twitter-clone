8b1f8d62d4ad26dae6f52c0497b9b4e896a0e39a
marthijn kip <marthijn1994@gmail.com>
2020-05-03 17:30:01 +0200

    Displaying correct tweet for retweeting with quote in the modal

--- resources/js/components/modals/appTweetRetweetModal.vue
+++ resources/js/components/modals/appTweetRetweetModal.vue
@@ -1,12 +1,21 @@
 <template>
     <div>
-        {{ tweet }}
+        <app-tweet-compose />
+
+        <component
+            v-if="tweet"
+            :is="`app-tweet-variant-${tweet.type}`"
+            :tweet="tweet"
+            class="border border-gray-700 rounded-lg mt-4 p-4"
+        />
     </div>
 </template>
 
 <script>
+    import AppTweetCompose from "../compose/AppTweetCompose";
     export default {
         name: "appTweetRetweetModal",
+        components: { AppTweetCompose },
         props: {
             tweet: {
                 required: true,
